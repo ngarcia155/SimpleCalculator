788150c8de8573468e10b1d7e76057d109944b03
Nathaniel Garcia <ngarcia15@unm.edu>
2023-06-07 16:29:20 -0600

    working Jar file

---
+++ src/main/java/calc/simplecalculator/Main.java
@@ -0,0 +1,10 @@
+package calc.simplecalculator;
+
+import calc.simplecalculator.CalcStarter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalcStarter.launch(CalcStarter.class, args);
+    }
+}

ffeb2f1b613df10c4e4c321300fe1b22571225c0
Nathaniel Garcia <ngarcia15@unm.edu>
2023-06-07 13:45:47 -0600

    creating GUI from sceneBuilder

--- src/main/java/calc/simplecalculator/HelloApplication.java
+++ src/main/java/calc/simplecalculator/HelloApplication.java
@@ -2,7 +2,9 @@ package calc.simplecalculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +13,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //Parent root = fxmlLoader.load();
+
+        Pane root = (Pane) fxmlLoader.load();
+        //root.setPrefSize(320, 240);
+
+
+        Scene scene = new Scene(root);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
